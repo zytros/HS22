8fea4a5ed4680ecfdcb1cfef1a5f887edc095727
Luca Sichi <lsichi@student.ethz.ch>
2022-10-11 22:43:51 +0200

    aaaaa

--- HPC/hpcsei-fall-2022-lecture/homeworks/hw01/skeleton_code/q4_cache_matrices/hadamard.cpp
+++ HPC/hpcsei-fall-2022-lecture/homeworks/hw01/skeleton_code/q4_cache_matrices/hadamard.cpp
@@ -16,11 +16,15 @@ void hadamard_col_wise(double* A, double* B, double* C, int N){
     for (size_t i = 0; i < N*N; i++){
         size_t ii = i%N;
         size_t j = i/N;
-        C[j*N+ii] = A[j*N+ii] * B[j*N+ii]
+        C[j*N+ii] = A[j*N+ii] * B[j*N+ii];
     }
 
     // TODO: Hadamard product col-wise
 
+}
+
+double randDouble(){
+    return (double)rand();
 }
 
 int main(int argc, char *argv[]){
@@ -39,7 +43,11 @@ int main(int argc, char *argv[]){
     double* A = (double*) calloc(N*N, sizeof(double));
     double* B = (double*) calloc(N*N, sizeof(double));
     double* C = (double*) calloc(N*N, sizeof(double)); //calloc = malloc but set to zeros
-    
+
+    for(size_t i = 0; i < N; i++){
+        A[i] = randDouble();
+        B[i] = randDouble();
+    }    
 
     const int reps = 1e3;
 
@@ -47,6 +55,10 @@ int main(int argc, char *argv[]){
     // TODO: benchmark your hadamard_row_wise function using std::chrono and with reps=1e3. 
     // Save the time in the variable "time_row". Measure in seconds!
 
+    const std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
+    hadamard_row_wise(A, B, C, N);
+    const std::chrono::time_point<std::chrono::steady_clock> end = std::chrono::steady_clock::now();
+    double time_row = std::chrono::duration_cast<std::chrono::seconds>(end-start).count();
     printf("Hadamard row-wise time %9.4fs\n", time_row);
 
     double* C_temp = (double*) calloc(N*N, sizeof(double));
@@ -58,8 +70,12 @@ int main(int argc, char *argv[]){
     // TODO: benchmark your hadamard_col_wise function using std::chrono and with reps=1e3.
     // Save the time in the variable "time_col". Measure in seconds!
 
+    const std::chrono::time_point<std::chrono::steady_clock> start_c = std::chrono::steady_clock::now();
+    hadamard_col_wise(A, B, C, N);
+    const std::chrono::time_point<std::chrono::steady_clock> end_c = std::chrono::steady_clock::now();
+    double time_col = std::chrono::duration_cast<std::chrono::seconds>(end_c-start_c).count();
 
-    printf("Hadamard row-wise time %9.4fs\n", time_col);
+    printf("Hadamard col-wise time %9.4fs\n", time_col);
 
     double error = 0.0;
     for(int i = 0; i < N*N; i++){
@@ -79,6 +95,6 @@ int main(int argc, char *argv[]){
     free(A);
     free(B);
     free(C);
-
+    free(C_temp);
     return 0;
 }
